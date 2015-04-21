8f47c1d1084f4b01db769750262a190b8538d586
tinybug <tinybug1991@gmail.com>
2015-04-21 23:13:48 +0800

    Update SimpleTimeClient.c

--- SimpleTimeClient.c
+++ SimpleTimeClient.c
@@ -22,50 +22,50 @@ static void err_doit(int, int, const char *, va_list);
 
 void err_sys(const char *fmt, ...)
 {
-	va_list		ap;
+   va_list	ap;
 
-	va_start(ap, fmt);
-	err_doit(1, LOG_ERR, fmt, ap);
-	va_end(ap);
-	exit(1);
+   va_start(ap, fmt);
+   err_doit(1, LOG_ERR, fmt, ap);
+   va_end(ap);
+   exit(1);
 }
 
 void err_quit(const char *fmt, ...)
 {
-	va_list		ap;
+   va_list ap;
 
-	va_start(ap, fmt);
-	err_doit(0, LOG_ERR, fmt, ap);
-	va_end(ap);
-	exit(1);
+   va_start(ap, fmt);
+   err_doit(0, LOG_ERR, fmt, ap);
+   va_end(ap);
+   exit(1);
 }
 
 static void err_doit(int errnoflag, int level, const char *fmt, va_list ap)
 {
-	int		errno_save, n;
-	char	buf[MAXLINE];
+   int	errno_save, n;
+   char buf[MAXLINE];
 
-	errno_save = errno;
+   errno_save = errno;
 #ifdef	HAVE_VSNPRINTF
-	vsnprintf(buf, sizeof(buf), fmt, ap);
+   vsnprintf(buf, sizeof(buf), fmt, ap);
 #else
-	vsprintf(buf, fmt, ap);
+   vsprintf(buf, fmt, ap);
 #endif
-	n = strlen(buf);
-	if (errnoflag)
-	{
-        snprintf(buf+n, sizeof(buf)-n, ": %s", strerror(errno_save));
-	}
-	strcat(buf, "\n");
+   n = strlen(buf);
+   if (errnoflag)
+   {
+       snprintf(buf+n, sizeof(buf)-n, ": %s", strerror(errno_save));
+   }
+   strcat(buf, "\n");
 
-	if (daemon_proc) {
-		syslog(level, buf);
-	} else {
-		fflush(stdout);
-		fputs(buf, stderr);
-		fflush(stderr);
-	}
-	return;
+   if (daemon_proc) {
+       syslog(level, buf);
+   } else {
+       fflush(stdout);
+       fputs(buf, stderr);
+       fflush(stderr);
+   }
+   return;
 }
 
 int main(int argc, char **argv)
