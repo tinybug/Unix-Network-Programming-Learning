e7c5705854e887130cbb40d5cbe585270f0fd588
tinybug <tinybug1991@gmail.com>
2015-04-21 23:06:59 +0800

    Create SimpleTimeClient.c

---
+++ SimpleTimeClient.c
@@ -0,0 +1,114 @@
+/*
+* simple time client
+* test time server: 203.117.180.36
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include <errno.h>
+
+#define	MAXLINE	4096
+
+int daemon_proc = 0;
+
+static void	err_doit(int, int, const char *, va_list);
+
+void err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void err_quit(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+static void err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int		errno_save, n;
+	char	buf[MAXLINE];
+
+	errno_save = errno;
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, sizeof(buf), fmt, ap);
+#else
+	vsprintf(buf, fmt, ap);
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+	{
+        snprintf(buf+n, sizeof(buf)-n, ": %s", strerror(errno_save));
+	}
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        err_quit("usage: programname <IPaddress>\n");
+    }
+
+    int sockfd;
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        err_sys("socket error\n");
+    }
+
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(13);
+    if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 0)
+    {
+        err_quit("inet_pton error");
+    }
+
+    if(connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
+    {
+        err_sys("connect error");
+    }
+
+    int n = 0;
+    char recvline[MAXLINE];
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0)
+    {
+        recvline[n] = 0;
+        if (fputs(recvline, stdout) == EOF)
+        {
+            err_sys("fputs error\n");
+        }
+    }
+    if (n < 0)
+    {
+        err_sys("read error");
+    }
+
+    return 0;
+}
